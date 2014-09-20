729fe755a5be8c24b2f5905bc7e4b4297833a946
juancarlospaco <juancarlospaco@gmail.com>
2014-09-20 17:10:23 -0300

    Add local server to develop

--- classmate/classmate.py
+++ classmate/classmate.py
@@ -1,8 +1,54 @@
-import webapp2
+# -*- coding: utf-8 -*-
+#
 
-class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
 
-application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
+"""Simple server that uses Google App Or Local Pythons BaseHTTPServer."""
+
+
+# imports
+import sys
+import os
+try:
+    import webapp2
+except Exception as error:
+    webapp2 = None
+    print(error)
+
+
+###############################################################################
+
+
+def run_local():
+    """This function runs a basic builtin server locally for development."""
+    try:
+        os.nice(19)
+    except Exception as error:
+        print(error)
+    if sys.version < '3':
+        import BaseHTTPServer as server
+        from CGIHTTPServer import CGIHTTPRequestHandler
+    else:
+        import http.server as server
+        from http.server import CGIHTTPRequestHandler
+    server_address, handler = ('', 8000), CGIHTTPRequestHandler
+    httpd = server.HTTPServer(server_address, handler)
+    print(("""Server running on port http://localhost:{}.
+           """.format(server_address[1])))
+    httpd.serve_forever()
+
+
+def run_on_the_cloud():
+    """This function runs a Google App server on the cloud for development."""
+    class MainPage(webapp2.RequestHandler):
+        def get(self):
+            self.response.headers['Content-Type'] = 'text/plain'
+
+    application = webapp2.WSGIApplication([('/', MainPage), ], debug=True)
+
+
+###############################################################################
+
+
+if __name__ in '__main__':
+    print(__doc__)
+    run_local() if not webapp2 else run_on_the_cloud()
