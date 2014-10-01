85f0d0a8bd9e2010b43a885b6c594b4bb147b164
kikocorreoso <kikocorreoso@gmail.com>
2014-10-01 15:37:42 +0200

    fix pyschool_local.py

--- pyschool/pyschool_local.py
+++ pyschool/pyschool_local.py
@@ -16,21 +16,21 @@ except Exception as error:
     webapp2 = None
     print(error)
 
-sys.path.append('libs')
-import CommandHandler
-import GoogleDataStore
+#sys.path.append('libs')
+#import CommandHandler
+#import GoogleDataStore
 
 # port to be used if the server runs locally
 if webapp2 is None:
-  parser = argparse.ArgumentParser()
-  parser.add_argument('--port', 
-    help="The port to be used by the local server")
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', 
+        help="The port to be used by the local server")
+    args = parser.parse_args()
 
-  if args.port:
-	port = int(args.port)
-  else:
-	port = 8000
+    if args.port:
+	    port = int(args.port)
+    else:
+	    port = 8000
 
 
 ###############################################################################
@@ -96,5 +96,5 @@ def run_on_the_cloud():
 
 
 if __name__ in '__main__':
-    p0rint(__doc__)
+    print(__doc__)
     run_local() if not webapp2 else run_on_the_cloud()
